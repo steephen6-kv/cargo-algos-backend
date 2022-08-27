21da67bcdb50db06164f5dcf8ddf7d8d51029222
steephen6-kv <steephen@keyvalue.systems>
2022-08-27 23:03:56 +0530

    Trip Drivers listing API

--- src/app/controller/index.ts
+++ src/app/controller/index.ts
@@ -5,10 +5,16 @@
 import UserController from "./UserController";
 import { UserService } from "../service/UserService";
 import { UserDao } from "../repository/UserDao";
+import { RideDao } from "../repository/RideDao";
+import { RideService } from "../service/RideService";
+import RideController from "./RideController";
 
 const userDao = new UserDao();
-const employeeService = new UserService(userDao);
+const userService = new UserService(userDao);
+const rideDao = new RideDao();
+const rideService = new RideService(rideDao);
 
 export default [
-  new UserController(employeeService)
+  new UserController(userService),
+  new RideController(rideService)
 ];
